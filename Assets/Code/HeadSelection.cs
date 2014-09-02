d4b6b6116e81d6e6684ba6de34113607ce8a2b65
ricklove <rickandkarenlove@hotmail.com>
2014-09-02 06:13:34 +0200

    Add Head Selection

---
+++ Assets/Code/HeadSelection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadSelection : MonoBehaviour
+{
+
+    public Sprite head;
+
+    // Use this for initialization
+    void Awake()
+    {
+        var headRenderer = GetComponentInChildren<SpriteRenderer>();
+        headRenderer.sprite = head;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
